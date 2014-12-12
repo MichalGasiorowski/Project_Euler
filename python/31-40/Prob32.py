cc2aac8f3dd7b6e5296801fd8dd2b0825db1c0de
MGasiorowski <enkidupal@gmail.com>
2014-12-12 23:31:36 +0100

    restructure folders

--- python/31-40/Prob32.py
+++ python/31-40/Prob32.py
@@ -10,10 +10,31 @@
 ##HINT: Some products can be obtained in more than one way
 ##so be sure to only include it once in your sum.
 from time import clock
+import math
+from functools import lru_cache
+
+last_digits_impossible = set( ( (1,1), (1,2),(1,3),(1,4),(1,5),(1,6),(1,7),(1,8),(1,9),
+                               (2,2), (2,5), (2,6), (3,3), (3,5), (4,4), (4,5), (5,5),
+                               (5,6),(5,7), (5,8), (5,9) , (6, 6), (6,8), (7,7),
+                               (8,8), (9,9)))
+
+@lru_cache(maxsize=None)
+def is_pandigital(num):
+    digit_set = set()
+    i = num
+    while(i >= 1):
+        rem = i % 10
+        if rem in digit_set:
+            return False
+        digit_set.add(rem)
+        i = int(i / 10)
+    return True
+        
 
 def main():
     start = clock()
-    None
+    
+    
 
 if __name__ == "__main__":
     main()
