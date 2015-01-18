7c4581842d74c632d6e757eac5fd6a2273852fbb
MGasiorowski <enkidupal@gmail.com>
2015-01-18 01:06:47 +0100

    Problem 35 ( Circular primes ) finished

--- java/java_euler/src/com/goose/util/BigIntegerUtils.java
+++ java/java_euler/src/com/goose/util/BigIntegerUtils.java
@@ -22,7 +22,14 @@ public class BigIntegerUtils {
     }
 
     public static int[] getDigits(int num) {
-        return getDigits(BigInteger.valueOf(num));
+
+        String s = "" + num;
+
+        int[] ret = new int[s.length()];
+        for(int i =0; i< s.length(); i++) {
+            ret[i] = Character.getNumericValue(s.charAt(i));
+        }
+        return ret;
     }
 
     public static BigInteger factorial(int number) {
@@ -64,4 +71,30 @@ public class BigIntegerUtils {
     public static boolean isNumberFactorialyCurious(int num) {
         return factorial(getDigits(num)).compareTo(BigInteger.valueOf(num)) == 0;
     }
+
+    public static int[] rotations(int num) {
+
+        String s = "" + num;
+        int[] ret = new int[s.length()];
+        int curr;
+        for(int offset = 0; offset < s.length(); offset++) {
+            curr = Integer.valueOf(s.substring(offset, s.length()) + s.substring(0, offset));
+            ret[offset] = curr;
+        }
+        return ret;
+    }
+    public static BigInteger[] rotations(BigInteger num) {
+
+        String s = num.toString();
+        BigInteger[] ret = new BigInteger[s.length()];
+        BigInteger curr;
+        for(int offset = 0; offset < s.length(); offset++) {
+            curr = new BigInteger(s.substring(offset, s.length()) + s.substring(0, offset));
+            ret[offset] = curr;
+        }
+        return ret;
+    }
+
+
+
 }
---
+++ java/java_euler/src/com/goose/util/IntUtils.java
@@ -0,0 +1,20 @@
+package com.goose.util;
+
+/**
+ * Created by Michal on 1/18/2015.
+ */
+public class IntUtils {
+
+    public static int[] rotations(int num) {
+
+        String s = "" + num;
+        int[] ret = new int[s.length()];
+        int curr;
+        for(int offset = 0; offset < s.length(); offset++) {
+            curr = Integer.valueOf(s.substring(offset, s.length()) + s.substring(0, offset));
+            ret[offset] = curr;
+        }
+        return ret;
+    }
+
+}
